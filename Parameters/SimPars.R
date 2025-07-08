a5e50da53a5139ca72981cba68d863a77fa1c174
KenupCF <caio.kenup@gmail.com>
2025-07-08 18:40:29 +0100

    bkp

--- Parameters/SimPars.R
+++ Parameters/SimPars.R
@@ -1,3 +1,15 @@
 
-n_years<-50
-n_sims<-10
+model_pars$sim<-list()
+
+model_pars$sim$n_years<-50
+model_pars$sim$n_iter<-10
+model_pars$sim$parametric_uncertainty<-FALSE
+model_pars$sim$n_samples_quantile_function<-1e4
+
+# template for quantile extraction (0 to 1 means all quantiles are sampled, 0.5 means to always get the median)
+if(model_pars$sim$parametric_uncertainty){
+  qrunif_template<-data.frame(min=0.0,max=1.0,dist="unif")
+}else{
+  qrunif_template<-data.frame(min=0.5,max=0.5,dist="unif")
+}
+
